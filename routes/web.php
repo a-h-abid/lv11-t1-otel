909ec17586de107a3d03acc82d5675a6ccd6bf1c
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2025-02-18 00:54:35 +0600

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,17 @@
 
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
-use OpenTelemetry\API\Trace\Span;
 
 Route::get('/', function () {
+    Log::info('Welcome page visited', [
+        'user' => 'John Wick',
+    ]);
 
-    // Retrieve the current active span (ensure your OpenTelemetry middleware/tracer is set up)
-    $span = Span::getCurrent();
-
-    if ($span !== null && $span->getContext()->isValid()) {
-        // Add the log as an event on the current span
-        $span->addEvent('log', [
-            'level'   => 'DEBUG',
-            'message' => 'Yo yo',
-            'context' => json_encode(['key' => 'value']),
-        ]);
-    }
-    Log::info('Welcome page visited');
     return view('welcome');
+});
+
+Route::get('throwerr', function () {
+    throw new \Exception("Error Processing Request", 1);
+
+    return '';
 });
